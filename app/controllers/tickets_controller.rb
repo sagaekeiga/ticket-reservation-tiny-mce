2277b6eb728e8abbb46d9bcfec01f381e331f906
Keiga Sagae <sagae5.28rujeae@gmail.com>
2018-07-24 22:41:58 +0900

    feat: tickets#create

---
+++ app/controllers/tickets_controller.rb
@@ -0,0 +1,28 @@
+class TicketsController < ApplicationController
+  def new
+    @ticket = Ticket.new
+  end
+
+  def create
+    @ticket = Ticket.new(ticket_params)
+    if @ticket.save
+      redirect_to :tickets, notice: 'チケットを作成しました。'
+    else
+      render :new
+    end
+  end
+
+  def index
+  end
+
+  private
+
+  def ticket_params
+    params.require(:ticket).permit(
+      :title,     # チケット名
+      :body,      # チケットの内容
+      :number,    # チケット販売可能個数
+      :expired_at # チケットの予約締切日時
+    )
+  end
+end
