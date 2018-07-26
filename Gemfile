c1e94ae4fee103a265e7e1114bc30654215b8d7c
Keiga Sagae <sagae5.28rujeae@gmail.com>
2018-07-26 23:21:15 +0900

    feat: devise

--- Gemfile
+++ Gemfile
@@ -33,7 +33,7 @@ gem 'jbuilder', '~> 2.5'
 
 gem 'haml-rails'
 gem 'bootstrap-sass', '~> 3.3.7'
-
+gem 'devise'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  root 'welcome#index'
+  devise_for :users, path: 'users', controllers: {
+    registrations: 'users/registrations', # 登録
+    confirmations: 'users/confirmations', # 確認
+    sessions: 'users/sessions'            # ログイン
+  }
   resources :tickets, only: %i(new create index)
 end
